a162964085c61928a9ea9c6009cf3338bb5f8f73
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-05 23:05:07 +0530

    Routes and Components Created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Box } from "@mui/system";
+import Navbar from "./components/Navbar";
+import VideoDetail from "./components/VideoDetail";
+import ChannelDetail from "./components/ChannelDetail";
+import SearchDetail from "./components/SearchDetail";
+import Feed from "./components/Feed";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:id" element={<SearchDetail />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>;
+};
 
-export default App
+export default App;
