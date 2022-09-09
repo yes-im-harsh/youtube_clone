0429876e28970e161601cc13a148a7e13c6b7660
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-09 20:45:56 +0530

    Individual Videos are now playing

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+
+import { Video } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
-  return <div>VideoDetail</div>;
+  const [videoDetail, setVideoDetail] = useState("");
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
+      setVideoDetail(data.items[0])
+    );
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Box flex={1}>
+          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+            <ReactPlayer
+              url={`https://youtube.com/watch?v=${id}`}
+              className="react-player"
+              controls
+            />
+            
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
+  );
 };
 
 export default VideoDetail;
